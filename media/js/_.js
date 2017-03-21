24ae7f436a76972e01b9bdbfa0ffb1f82dcf0f6d
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-03-21 15:38:24 +0100

    version bump

--- media/js/_.js
+++ media/js/_.js
@@ -24,7 +24,7 @@ window.$ = window.$jq = jQuery.noConflict();
         LONG_NAME: "Documentation Search",
 
         // Version.
-        VERSION: '0.9.7.0',
+        VERSION: '0.9.7.1',
 
         // Copyright statement.
         copyrightYear: new Date().getFullYear(),
