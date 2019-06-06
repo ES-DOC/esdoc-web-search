2054f8d1d20927e66af60a1bcfadd8f7e89a8f44
momipsl <momipsl@ipsl.fr>
2019-06-06 15:55:21 +0200

    1. Version bump.

--- media/js/_.js
+++ media/js/_.js
@@ -24,7 +24,7 @@ window.$ = window.$jq = jQuery.noConflict();
         LONG_NAME: "Documentation Search",
 
         // Version.
-        VERSION: '0.13.2.0',
+        VERSION: '1.0.0',
 
         // Copyright statement.
         copyrightYear: new Date().getFullYear(),
